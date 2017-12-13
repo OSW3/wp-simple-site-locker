9d6ded95fdaeab9328fdc940f264961fcf5b4afb
Arnaud BODEL <contact@osw3.net>
2017-12-13 17:21:28 +0100

    -

--- views/form.php
+++ views/form.php
@@ -3,10 +3,10 @@
 if (!defined('ABSPATH')) {exit;}
 ?>
 
+<p><?= __( 'This website is temporarily inaccessible.', 'simple_site_locker'); ?></p>
 
 <form action="/" method="POST" novalidate>
     <?php wp_nonce_field('sitelocker', 'sitelocker-token'); ?>
-    <p><?= __( 'This website is temporarily inaccessible.', 'simple_site_locker'); ?></p>
     <input type="password" name="sitelocker-password" placeholder="Unlocker code">
     <br>
     <button type="submit">Unlock</button>
